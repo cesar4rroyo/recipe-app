3d154d1ab616f7baa491ad1a10a1b36b34cd7aaf
Cesar Arroyo <cesardavid_09@hotmail.com>
2020-03-17 18:31:24 -0500

    Updates

---
+++ precache-manifest.76d6b2648d3d9c557f54c365a1cd9431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883be7a95326e8ce91a21f21007190b5",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "8f70e0ca3dbc98095e9b",
+    "url": "/recipe-app/static/css/main.e0ca9bda.chunk.css"
+  },
+  {
+    "revision": "bb9daf8a716f6cc72799",
+    "url": "/recipe-app/static/js/2.33cf706a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/recipe-app/static/js/2.33cf706a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f70e0ca3dbc98095e9b",
+    "url": "/recipe-app/static/js/main.92e93da9.chunk.js"
+  },
+  {
+    "revision": "887dbc60425b31b5d4ea",
+    "url": "/recipe-app/static/js/runtime-main.2006017e.js"
+  }
+]);
